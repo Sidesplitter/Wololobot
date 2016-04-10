6bf1649a706dd1e9dde54974358b2c0f081b8733
Jord Nijhuis <jord.nijhuis@jordev.nl>
2016-04-10 12:25:48 +0200

    Added basic functionality for the tauntpack class
    Signed-off-by: Jord Nijhuis <@jord.nijhuis>

--- src/tauntpack.py
+++ src/tauntpack.py
@@ -1,5 +1,44 @@
+import os
+
 class TauntPack:
 
     name = ""
     description = ""
     taunts = []
+    path = ""
+
+    def __init__(self, name: str, description: str, path: str):
+
+        if(name == ""):
+            raise Exception("Name may not be empty")
+
+        if(description == ""):
+            raise Exception("Description may not be empty")
+
+        if(path == ""):
+            raise Exception("Path may not be empty")
+
+        if(os.path.isdir(path) == False):
+            raise Exception("Directory does not exist")
+
+        if(os.path.isfile(path + "/config.ini") == False):
+            raise Exception("Directory does not contain a config.ini")
+
+        self.name = name
+        self.description = description
+        self.path = path
+
+
+    def findMatch(self, message: str):
+
+        """
+
+        :param message: The message to check
+        :return: None if no Taunt was found, otherwise the taunt
+        """
+        for taunt in self.taunts:
+
+            if(taunt.matches(message)):
+                return taunt
+
+        return None
